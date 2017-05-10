0ab6ff01c1a679c416b005542e563f88024229db
hoangnv <hoangnv.ctv@gmail.com>
2017-05-10 22:44:23 +0700

    Create project

---
+++ HDDFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HDDFilter
+//
+//  Created by Nguyễn Văn Hoàng on 5/10/17.
+//  Copyright © 2017 muvik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
